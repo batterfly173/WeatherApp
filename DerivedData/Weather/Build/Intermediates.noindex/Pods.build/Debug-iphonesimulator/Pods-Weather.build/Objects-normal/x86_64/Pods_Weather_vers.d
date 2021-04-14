64d9cf8411a5b53d9e993c276e52043d9cbf8620
Evgeny <evgenyf021@gmail.com>
2021-04-14 23:42:15 +0300

    delete DerivedData

--- DerivedData/Weather/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-Weather.build/Objects-normal/x86_64/Pods_Weather_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/evgeny/source/WeatherApp/DerivedData/Weather/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-Weather.build/DerivedSources/Pods_Weather_vers.c
--- DerivedData/Weather/Build/Intermediates.noindex/Weather.build/Debug-iphonesimulator/Weather.build/Script-B71CA9B57AA17AEBCBB01B8F.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${PODS_ROOT}/Target Support Files/Pods-Weather/Pods-Weather-frameworks.sh"
-
